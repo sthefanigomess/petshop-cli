bf69dedd86fc089dc4365cd8f59064840b992557
Sérgio Moura <smouracalmon@gmail.com>
2021-09-10 15:18:18 -0300

    Criando função descrever que exibe todas as informações do cachorro

--- funcoes.js
+++ funcoes.js
@@ -11,5 +11,26 @@ module.exports = {
             }
         )
         console.table(copiaDeCachorros);
+    },
+    descrever: function(pos) {
+
+        // Verificando se existe um cachorro cadastrado na posição pos
+        if(pos < 0 || pos >= cachorros.length){
+            console.log("Cachorro inexistente.");
+            return;
+        }
+
+        let cachorro = cachorros[pos];
+        console.log(`Nome: ${cachorro.nome} (${cachorro.sexo == "m"?"Macho":"Fêmea"})`);
+        console.log(`Data de Nascimento: ${cachorro.dataDeNascimento}`);
+        console.log(`Peso: ${cachorro.peso} kg`);
+        console.log(`Castrado: ${cachorro.castrado?"Sim":"Não"}`);
+        console.log('')
+        console.log("Vacinas");
+        console.table(cachorro.vacinas);
+        console.log('')
+        console.log("Serviços")
+        console.table(cachorro.servicos);
+
     }
 }
